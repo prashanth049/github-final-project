7053925cca22b7ca52247dee3207db67858a9832
prashanth049 <71222337+prashanth049@users.noreply.github.com>
2022-07-12 16:13:16 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: karri prashanth
    # Additional Authors:
-   # <your GitHub username>
+   # prashanth049
 
    # Input:
    # p, principal amount
